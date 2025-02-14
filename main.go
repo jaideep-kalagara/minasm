0df32d7336812eecf0cb408ae18ec0808a6a48ba
jaideep-kalagara <kalagarajaideep2013@gmail.com>
2025-02-14 10:29:34 -0800

    Added strings

--- main.go
+++ main.go
@@ -5,59 +5,95 @@ import (
 	"os"
 	"strconv"
 	"strings"
+
 	"github.com/jaideep-kalagara/minasm/util"
 )
 
 func main() {
-	if len(os.Args[1:]) == 0 {
+	if len(os.Args) < 2 {
 		fmt.Println("Please provide a file path")
 		os.Exit(1)
 	}
 
-	var fh util.FileHandler
+	// Ensure FileHandler and Stack are properly initialized
+	fh := util.FileHandler{}
+	stack := util.Stack{}
 
-	var stack util.Stack
 	program := fh.SplitFile(os.Args[1])
-	for i, v := range program {
-		opcode := strings.Split(v, " ")[0]
-		args := strings.Split(v, " ")[1:]
-		fmt.Println(i, opcode, args)
+	for _, v := range program {
+		parts := strings.Fields(v)
+		if len(parts) == 0 {
+			continue
+		}
+
+		opcode := parts[0]
+		args := parts[1:]
+
 		switch opcode {
 		case "ADD":
-			a, _ := stack.Pop()
-			b, ok := stack.Pop()
-			if !ok {
-				fmt.Println("Not enough operands")
+			a, ok1 := stack.Pop()
+			b, ok2 := stack.Pop()
+			if !ok1 || !ok2 {
+				fmt.Println("Not enough operands for ADD")
 				os.Exit(1)
 			}
 			stack.Push(a + b)
+
 		case "PUSH":
+			if len(args) == 0 {
+				fmt.Println("Missing argument for PUSH")
+				os.Exit(1)
+			}
 			val, err := strconv.Atoi(args[0])
 			if err != nil {
-				fmt.Println("Invalid value")
+				fmt.Println("Invalid PUSH value:", args[0])
 				os.Exit(1)
 			}
 			stack.Push(val)
+
 		case "SUB":
-			a, _ := stack.Pop()
-			b, ok := stack.Pop()
-			if !ok {
-				fmt.Println("Not enough operands")
+			a, ok1 := stack.Pop()
+			b, ok2 := stack.Pop()
+			if !ok1 || !ok2 {
+				fmt.Println("Not enough operands for SUB")
 				os.Exit(1)
 			}
 			stack.Push(b - a)
+
 		case "POP":
-			if len(stack.Stack) == 0 {
+			val, ok := stack.Pop()
+			if !ok {
 				fmt.Println("Stack is empty")
 				os.Exit(1)
 			}
-			val, ok := stack.Pop()
-			if !ok {
-				fmt.Println("Error popping from stack")
-				os.Exit(1)
-			}
 			fmt.Println(val)
+		case "POPSTR":
+			val, _ := strconv.Atoi(args[0])
+			str := stack.PopString(val)
+			fmt.Println(str)
+		case "PRINT":
+			if strings.Contains(v, "\"") {
+				parts := strings.SplitN(v, "\"", 2)
+				if len(parts) > 1 {
+					fmt.Println(parts[1])
+				} else {
+					fmt.Println("Invalid PRINT statement")
+				}
+			} else {
+				fmt.Println("Invalid PRINT format")
+			}
+
+		case "EXIT":
+			os.Exit(0)
+
+		case "READ":
+			var input string
+			fmt.Scan(&input) // Use &input to correctly read user input
+			stack.PushString(input)
+
+		default:
+			fmt.Printf("Unknown opcode: %s\n", opcode)
+			os.Exit(1)
 		}
 	}
-
 }
--- util/stack.go
+++ util/stack.go
@@ -26,3 +26,22 @@ func (s *Stack) Peek() (int, bool) {
 	}
 	return s.Stack[s.top-1], true
 }
+
+func (s *Stack) PushString(str string) {
+	for _, char := range str {
+		s.Push(int(char)) // Store ASCII value of each character
+	}
+}
+
+// Convert ASCII values back to a string
+func (s *Stack) PopString(length int) string {
+	var result []rune
+	for i := 0; i < length; i++ {
+		val, ok := s.Pop()
+		if !ok {
+			break
+		}
+		result = append([]rune{rune(val)}, result...) // Reverse order since it's a stack
+	}
+	return string(result)
+}
